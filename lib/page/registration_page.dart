15a8ba250fcab19d882e56cb4d29800552438ec7
jiangyaguang <1548336446@qq.com>
2022-02-24 15:45:46 +0800

    feat: 增加扫码功能

--- lib/page/registration_page.dart
+++ lib/page/registration_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bili_app/model/form_deploy.dart';
 import 'package:flutter_bili_app/util/color.dart';
 import 'package:flutter_bili_app/util/toast.dart';
 import 'package:flutter_bili_app/widget/appbar.dart';
 import 'package:flutter_bili_app/widget/login_input.dart';
+import 'package:flutter_easy_permission/constants.dart';
+import 'package:flutter_easy_permission/flutter_easy_permission.dart';
+import 'package:flutter_scankit/flutter_scankit.dart';
+import 'customized_view.dart';
+
+//权限
+const _permissionGroup = const [PermissionGroup.Camera, PermissionGroup.Photos];
+
+//权限
+const _permissions = const [
+  Permissions.READ_EXTERNAL_STORAGE,
+  Permissions.CAMERA
+];
 
 ///注册页面
 class RegistrationPage extends StatefulWidget {
@@ -18,6 +32,10 @@ class RegistrationPage extends StatefulWidget {
 class _RegistrationPageState extends State<RegistrationPage> {
   List<FormDeploy> dataList = []; //接口返回的配置信息
   Map dataForm = {}; //表达提交的数据
+  FlutterScankit scanKit;
+  String code = "123";
+  bool isCustom;
+
   @override
   void initState() {
     super.initState();
@@ -43,7 +61,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
         "isRequired": false,
         "field": "merchant",
         "verifyMsg": "仓库编码不能为空"
-      },{
+      },
+      {
         "label": "商家名称",
         "placeholder": '请输入仓库编码',
         "cnpId": 1,
@@ -53,7 +72,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
         "isRequired": false,
         "field": "merchant",
         "verifyMsg": "仓库编码不能为空"
-      },{
+      },
+      {
         "label": "商家名称",
         "placeholder": '请输入仓库编码',
         "cnpId": 1,
@@ -72,6 +92,21 @@ class _RegistrationPageState extends State<RegistrationPage> {
       }
     });
     print("123 ${dataList[0].label}");
+
+    ///
+    ///
+    scanKit = FlutterScankit();
+    scanKit.addResultListen((val) {
+      setState(() {
+        code = val;
+      });
+    });
+
+    FlutterEasyPermission().addPermissionCallback(
+        onGranted: (requestCode, perms, perm) {
+          isCustom ? newPage(context) : startScan();
+        },
+        onDenied: (requestCode, perms, perm, isPermanent) {});
   }
 
   @override
@@ -111,7 +146,18 @@ class _RegistrationPageState extends State<RegistrationPage> {
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(4)),
                         height: 40,
-                        onPressed: () {},
+                        onPressed: () async {
+                          isCustom = false;
+                          if (!await FlutterEasyPermission.has(
+                              perms: _permissions,
+                              permsGroup: _permissionGroup)) {
+                            FlutterEasyPermission.request(
+                                perms: _permissions,
+                                permsGroup: _permissionGroup);
+                          } else {
+                            startScan();
+                          }
+                        },
                         disabledColor: primary[50],
                         color: Colors.grey,
                         child: Text('取消',
@@ -131,6 +177,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
                         child: Text('提交',
                             style:
                                 TextStyle(color: Colors.white, fontSize: 16))),
+                    SizedBox(width: 20),
+                    Text(code)
                   ],
                 ),
               )
@@ -141,6 +189,22 @@ class _RegistrationPageState extends State<RegistrationPage> {
     );
   }
 
+  Future<void> startScan() async {
+    try {
+      await scanKit.startScan(scanTypes: [ScanTypes.ALL]);
+    } on PlatformException {}
+  }
+
+  Future<void> newPage(BuildContext context) async {
+    var code = await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return CustomizedView();
+    }));
+
+    setState(() {
+      this.code = code ?? "";
+    });
+  }
 
   void send() async {
     showToast('校验通过');
@@ -151,7 +215,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
       print("${item.field},333333");
       if (item.isRequired) {
         if (dataForm[item.field] != null) {
-          if (dataForm[item.field]!='') {
+          if (dataForm[item.field] != '') {
           } else {
             showWarnToast("${item.verifyMsg}");
             return false;
